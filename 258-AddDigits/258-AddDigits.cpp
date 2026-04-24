fbb5661a4b3a3e288920bb907895881d7f14e508
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-04-24 17:06:23 +0530

    Create 258-AddDigits

---
+++ 258-AddDigits/258-AddDigits.cpp
@@ -0,0 +1,18 @@
+// Last updated: 4/24/2026, 5:06:22 PM
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        while(num>9){
+        int ans = 0, rem;
+        
+        while(num!=0){
+            rem = num%10;
+            num/=10;
+            ans+=rem;
+        }
+        num =ans;
+        }
+        return num;
+    }
+};
