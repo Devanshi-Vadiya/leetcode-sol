62be15f1eaa88c5c88eae7f2c682604f4cbf6a25
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-09 21:54:55 +0530

    File updated at 5/9/2026, 9:54:53 PM

--- 136-SingleNumber/136-SingleNumber.cpp
+++ 136-SingleNumber/136-SingleNumber.cpp
@@ -1,13 +1,19 @@
-// Last updated: 5/9/2026, 9:45:43 PM
+// Last updated: 5/9/2026, 9:54:53 PM
 1class Solution {
 2public:
 3    int singleNumber(vector<int>& nums) {
-4        int ans = 0;
+4        unordered_map<int, int> freq;
 5
 6        for (int num : nums) {
-7            ans ^= num;
+7            freq[num]++;
 8        }
 9
-10        return ans;
-11    }
-12};
+10        for (auto it : freq) {
+11            if (it.second == 1) {
+12                return it.first;
+13            }
+14        }
+15
+16        return -1;
+17    }
+18};
