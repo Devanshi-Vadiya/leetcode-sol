c00815dfcf3e909fc3dd87ad14212000653d3305
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-09 22:45:34 +0530

    Create 287-FindtheDuplicateNumber

---
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.cpp
@@ -0,0 +1,17 @@
+// Last updated: 5/9/2026, 10:45:32 PM
+1class Solution {
+2public:
+3    int findDuplicate(vector<int>& nums) {
+4
+5        sort(nums.begin(), nums.end());
+6
+7        for (int i = 1; i < nums.size(); i++) {
+8
+9            if (nums[i] == nums[i - 1]) {
+10                return nums[i];
+11            }
+12        }
+13
+14        return -1;
+15    }
+16};
