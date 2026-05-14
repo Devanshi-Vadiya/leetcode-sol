31ef57b1e7733fe9cd1545f5907a45fe724ea191
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-14 16:53:31 +0530

    File updated at 5/14/2026, 4:53:31 PM

--- 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.cpp
+++ 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.cpp
@@ -1,14 +1,19 @@
-// Last updated: 5/14/2026, 4:48:51 PM
+// Last updated: 5/14/2026, 4:53:31 PM
 1class Solution {
 2public:
-3    vector<int> shuffle(vector<int>& nums, int n) {
-4        vector<int> ans;
+3    int maximumWealth(vector<vector<int>>& accounts) {
+4        int maxWealth = 0;
 5
-6        for(int i = 0; i < n; i++) {
-7            ans.push_back(nums[i]);
-8            ans.push_back(nums[i + n]);
-9        }
-10
-11        return ans;
-12    }
-13};
+6        for(int i = 0; i < accounts.size(); i++) {
+7            int sum = 0;
+8
+9            for(int j = 0; j < accounts[i].size(); j++) {
+10                sum += accounts[i][j];
+11            }
+12
+13            maxWealth = max(maxWealth, sum);
+14        }
+15
+16        return maxWealth;
+17    }
+18};
