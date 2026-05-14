ef4eeae24e76105302dd2245878d10ec49860b15
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-14 22:22:39 +0530

    File updated at 5/14/2026, 10:22:39 PM

--- 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.cpp
+++ 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.cpp
@@ -1,19 +1,23 @@
-// Last updated: 5/14/2026, 4:53:31 PM
+// Last updated: 5/14/2026, 10:22:39 PM
 1class Solution {
 2public:
-3    int maximumWealth(vector<vector<int>>& accounts) {
-4        int maxWealth = 0;
+3    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+4        int maxCandy = 0;
 5
-6        for(int i = 0; i < accounts.size(); i++) {
-7            int sum = 0;
-8
-9            for(int j = 0; j < accounts[i].size(); j++) {
-10                sum += accounts[i][j];
-11            }
-12
-13            maxWealth = max(maxWealth, sum);
-14        }
-15
-16        return maxWealth;
-17    }
-18};
+6        for(int i = 0; i < candies.size(); i++) {
+7            maxCandy = max(maxCandy, candies[i]);
+8        }
+9
+10        vector<bool> ans;
+11
+12        for(int i = 0; i < candies.size(); i++) {
+13            if(candies[i] + extraCandies >= maxCandy) {
+14                ans.push_back(true);
+15            } else {
+16                ans.push_back(false);
+17            }
+18        }
+19
+20        return ans;
+21    }
+22};
