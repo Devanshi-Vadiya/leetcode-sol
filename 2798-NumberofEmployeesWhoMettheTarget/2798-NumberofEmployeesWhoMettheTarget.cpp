858df5a12bacfd7f3bb2c913285f77aca02e760b
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-15 14:33:16 +0530

    File updated at 5/15/2026, 2:33:16 PM

--- 2798-NumberofEmployeesWhoMettheTarget/2798-NumberofEmployeesWhoMettheTarget.cpp
+++ 2798-NumberofEmployeesWhoMettheTarget/2798-NumberofEmployeesWhoMettheTarget.cpp
@@ -1,13 +1,18 @@
-// Last updated: 5/15/2026, 2:27:01 PM
+// Last updated: 5/15/2026, 2:33:16 PM
 1class Solution {
 2public:
-3    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
-4        int ans = 0;
-5        for(int i=0;i<hours.size();i++){
-6            if(hours[i]>=target){
-7                ans ++;
-8            }
-9        }
-10        return ans;
-11    }
-12};
+3    int mostWordsFound(vector<string>& sentences) {
+4        int maxWords =0;
+5        for(int i = 0;i < sentences.size();i++){
+6         int count = 1;
+7         for(int j = 0;j<sentences[i].size();j++){
+8            if(sentences[i][j] ==' '){
+9                count ++;
+10            }
+11         } 
+12         maxWords = max(maxWords,count);  
+13        }
+14
+15        return maxWords;
+16    }
+17};
