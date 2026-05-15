21b3b385cda82ee3b6eefa453937072cf3601d6e
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-15 14:54:08 +0530

    File updated at 5/15/2026, 2:54:08 PM

--- 2011-FinalValueofVariableAfterPerformingOperations/2011-FinalValueofVariableAfterPerformingOperations.cpp
+++ 2011-FinalValueofVariableAfterPerformingOperations/2011-FinalValueofVariableAfterPerformingOperations.cpp
@@ -1,18 +1,17 @@
-// Last updated: 5/15/2026, 2:53:28 PM
+// Last updated: 5/15/2026, 2:54:08 PM
 1class Solution {
 2public:
 3    int finalValueAfterOperations(vector<string>& operations) {
-4        int ans = 0;
+4        int x = 0;
 5
-6        for(int i = 0; i < operations.size(); i++) {          
-7            if(operations[i] == "--X" || operations[i] == "X--") {
-8                ans--;
-9            }
-10            else {
-11                ans++;
-12            }
-13        }
-14
-15        return ans;
-16    }
-17};
+6        for(int i = 0; i < operations.size(); i++) {
+7            if(operations[i][1] == '+') {
+8                x++;
+9            } else {
+10                x--;
+11            }
+12        }
+13
+14        return x;
+15    }
+16};
