76890f618bc5dcf0a9c782b5c5185369d239f2bd
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-15 14:46:41 +0530

    File updated at 5/15/2026, 2:46:41 PM

--- 2798-NumberofEmployeesWhoMettheTarget/2798-NumberofEmployeesWhoMettheTarget.cpp
+++ 2798-NumberofEmployeesWhoMettheTarget/2798-NumberofEmployeesWhoMettheTarget.cpp
@@ -1,18 +1,21 @@
-// Last updated: 5/15/2026, 2:35:48 PM
+// Last updated: 5/15/2026, 2:46:41 PM
 1class Solution {
 2public:
 3    int mostWordsFound(vector<string>& sentences) {
-4        int maxWords =0;
-5        for(int i = 0;i < sentences.size();i++){
-6         int count = 1;
-7         for(int j = 0;j<sentences[i].size();j++){
-8            if(sentences[i][j] ==' '){
-9                count ++;
-10            }
-11         } 
-12         maxWords = max(maxWords,count);  
-13        }
-14
-15        return maxWords;
-16    }
-17};
+4      int maxWords = 0;
+5
+6      for(int i =0;i<sentences.size();i++){
+7
+8        int count = 1; //no. of words = spaces + 1
+9
+10        for(int j=0;j<sentences[i].size();j++){
+11            if(sentences[i][j] == ' '){
+12                count ++;
+13            }
+14        }
+15            maxWords = max(maxWords, count);
+16
+17      }  
+18      return maxWords;
+19    }
+20};
