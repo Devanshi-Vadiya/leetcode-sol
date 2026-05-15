5a0719a495879f914152617439088d493949764e
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-15 15:02:20 +0530

    File updated at 5/15/2026, 3:02:20 PM

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.cpp
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.cpp
@@ -1,13 +1,23 @@
-// Last updated: 5/15/2026, 2:57:55 PM
+// Last updated: 5/15/2026, 3:02:20 PM
 1class Solution {
 2public:
-3    vector<int> getConcatenation(vector<int>& nums) {
-4        vector<int> ans = nums;
+3    int arraySign(vector<int>& nums) {
+4        int negativeCount = 0;
 5
 6        for(int i = 0; i < nums.size(); i++) {
-7            ans.push_back(nums[i]);
-8        }
-9
-10        return ans;
-11    }
-12};
+7            if(nums[i] == 0) {
+8                return 0;
+9            }
+10
+11            if(nums[i] < 0) {
+12                negativeCount++;
+13            }
+14        }
+15
+16        if(negativeCount % 2 == 0) {
+17            return 1;
+18        }
+19
+20        return -1;
+21    }
+22};
