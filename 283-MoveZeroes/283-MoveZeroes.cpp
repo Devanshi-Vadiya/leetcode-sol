0cbe7cd328a3445e0323a771c1f67b66024ee106
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-16 11:45:39 +0530

    File updated at 5/16/2026, 11:45:38 AM

--- 283-MoveZeroes/283-MoveZeroes.cpp
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -1,18 +1,19 @@
-// Last updated: 5/16/2026, 11:40:15 AM
+// Last updated: 5/16/2026, 11:45:38 AM
 1class Solution {
 2public:
-3    void moveZeroes(vector<int>& nums) {
-4
-5        int nonZeroIndex=0;
-6
-7        for(int i =0;i<nums.size();i++){
-8            if(nums[i]!=0){
-9                nums[nonZeroIndex] = nums[i];
-10                nonZeroIndex ++;
-11            }
-12        }
-13        for(int i = nonZeroIndex ; i< nums.size();i++){
-14            nums[i] = 0;
+3    bool isPalindrome(int x) {
+4        if(x<0){
+5            return false;
+6        }
+7
+8        long long originalNumber = x;
+9        long long reversedNumber =0;
+10
+11        while(x>0){
+12            int lastDigit =x%10;
+13            reversedNumber = reversedNumber *10 + lastDigit;
+14            x = x / 10;
 15        }
-16    }
-17};
+16        return originalNumber == reversedNumber;
+17    }
+18};
