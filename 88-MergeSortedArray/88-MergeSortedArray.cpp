a813f757ae2f2fee622958cd54d059f8161a29b7
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-19 15:32:39 +0530

    File updated at 5/19/2026, 3:32:39 PM

--- 88-MergeSortedArray/88-MergeSortedArray.cpp
+++ 88-MergeSortedArray/88-MergeSortedArray.cpp
@@ -1,21 +1,20 @@
-// Last updated: 5/19/2026, 3:31:58 PM
+// Last updated: 5/19/2026, 3:32:39 PM
 1class Solution {
 2public:
-3    int sumOfUnique(vector<int>& nums) {
-4        unordered_map<int, int> frequency;
+3    int numJewelsInStones(string jewels, string stones) {
+4        unordered_set<char> jewelSet;
 5
-6        for (int i = 0; i < nums.size(); i++) {
-7            frequency[nums[i]]++;
+6        for(int i =0;i<jewels.size();i++){
+7            jewelSet.insert(jewels[i]);
 8        }
 9
-10        int sum = 0;
+10        int count = 0;
 11
-12        for (int i = 0; i < nums.size(); i++) {
-13            if (frequency[nums[i]] == 1) {
-14                sum += nums[i];
+12        for(int i =0;i<stones.size();i++){
+13            if(jewelSet.count(stones[i])){
+14                count++;
 15            }
 16        }
-17
-18        return sum;
-19    }
-20};
+17        return count;
+18    }
+19};
