a4f329e79380c67db8eddd9170f0ff43686298eb
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-20 17:03:48 +0530

    File updated at 5/20/2026, 5:03:49 PM

--- 977-SquaresofaSortedArray/977-SquaresofaSortedArray.cpp
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.cpp
@@ -1,27 +1,22 @@
-// Last updated: 5/20/2026, 4:57:28 PM
+// Last updated: 5/20/2026, 5:03:49 PM
 1class Solution {
 2public:
-3    vector<int> sortedSquares(vector<int>& nums) {
-4        int n = nums.size();
+3    int findNumbers(vector<int>& nums) {
+4        int answer = 0;
 5
-6        vector<int> answer(n);
-7
-8        int left = 0;
-9        int right = n - 1;
-10        int index = n - 1;
-11
-12        while (left <= right) {
-13            if (abs(nums[left]) > abs(nums[right])) {
-14                answer[index] = nums[left] * nums[left];
-15                left++;
-16            } else {
-17                answer[index] = nums[right] * nums[right];
-18                right--;
-19            }
-20
-21            index--;
-22        }
-23
-24        return answer;
-25    }
-26};
+6        for (int num : nums) {
+7            int digits = 0;
+8
+9            while (num > 0) {
+10                digits++;
+11                num = num / 10;
+12            }
+13
+14            if (digits % 2 == 0) {
+15                answer++;
+16            }
+17        }
+18
+19        return answer;
+20    }
+21};
