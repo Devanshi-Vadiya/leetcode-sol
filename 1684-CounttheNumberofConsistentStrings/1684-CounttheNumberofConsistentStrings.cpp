c35ae62f25b51285f60c50d5b6da2d5e4db780d9
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-21 22:17:22 +0530

    File updated at 5/21/2026, 10:17:24 PM

--- 1684-CounttheNumberofConsistentStrings/1684-CounttheNumberofConsistentStrings.cpp
+++ 1684-CounttheNumberofConsistentStrings/1684-CounttheNumberofConsistentStrings.cpp
@@ -1,11 +1,23 @@
-// Last updated: 5/21/2026, 10:16:40 PM
+// Last updated: 5/21/2026, 10:17:24 PM
 1class Solution {
 2public:
-3    int findLUSlength(string a, string b) {
-4        if(a == b) {
-5            return -1;
-6        }
-7
-8        return max(a.length(), b.length());
-9    }
-10};
+3    bool checkRecord(string s) {
+4        int absent = 0;
+5
+6        for(int i = 0; i < s.length(); i++) {
+7            if(s[i] == 'A') {
+8                absent++;
+9            }
+10
+11            if(absent >= 2) {
+12                return false;
+13            }
+14
+15            if(i >= 2 && s[i] == 'L' && s[i - 1] == 'L' && s[i - 2] == 'L') {
+16                return false;
+17            }
+18        }
+19
+20        return true;
+21    }
+22};
