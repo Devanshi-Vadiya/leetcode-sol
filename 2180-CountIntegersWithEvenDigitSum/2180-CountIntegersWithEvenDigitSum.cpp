0d38a6e83c395dd6ae04d4307b3c9ccfe2ea8bb4
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-21 20:42:36 +0530

    File updated at 5/21/2026, 8:42:38 PM

--- 2180-CountIntegersWithEvenDigitSum/2180-CountIntegersWithEvenDigitSum.cpp
+++ 2180-CountIntegersWithEvenDigitSum/2180-CountIntegersWithEvenDigitSum.cpp
@@ -1,23 +1,19 @@
-// Last updated: 5/21/2026, 8:39:49 PM
+// Last updated: 5/21/2026, 8:42:38 PM
 1class Solution {
 2public:
-3    int countEven(int num) {
-4        int count = 0;
-5
-6        for (int i = 1; i <= num; i++) {
-7            int n = i;
-8            int sum = 0;
+3    int subtractProductAndSum(int n) {
+4        int sum = 0;
+5        int product = 1;
+6
+7        while (n > 0) {
+8            int digit = n % 10;
 9
-10            while (n > 0) {
-11                sum += n % 10;
-12                n = n / 10;
-13            }
-14
-15            if (sum % 2 == 0) {
-16                count++;
-17            }
-18        }
-19
-20        return count;
-21    }
-22};
+10            sum += digit;
+11            product *= digit;
+12
+13            n = n / 10;
+14        }
+15
+16        return product - sum;
+17    }
+18};
