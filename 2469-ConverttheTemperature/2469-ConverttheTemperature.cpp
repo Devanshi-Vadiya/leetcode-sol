f37f8ff9390e2958d1426b091e097854a36d1810
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-23 21:11:23 +0530

    File updated at 5/23/2026, 9:11:23 PM

--- 2469-ConverttheTemperature/2469-ConverttheTemperature.cpp
+++ 2469-ConverttheTemperature/2469-ConverttheTemperature.cpp
@@ -1,10 +1,21 @@
-// Last updated: 5/23/2026, 9:10:08 PM
+// Last updated: 5/23/2026, 9:11:23 PM
 1class Solution {
 2public:
-3    vector<double> convertTemperature(double celsius) {
-4        double kelvin = celsius + 273.15;
-5        double fahrenheit = (celsius * 1.80) + 32.00;
-6
-7        return {kelvin, fahrenheit};
-8    }
-9};
+3    string destCity(vector<vector<string>>& paths) {
+4        unordered_set <string> start;
+5
+6        for(int i = 0; i < paths.size(); i++) {
+7            start.insert(paths[i][0]);
+8        }
+9
+10        for(int i = 0; i < paths.size(); i++) {
+11            string city = paths[i][1];
+12
+13            if(start.find(city) == start.end()) {
+14                return city;
+15            }
+16        }
+17
+18        return "";
+19    }
+20};
