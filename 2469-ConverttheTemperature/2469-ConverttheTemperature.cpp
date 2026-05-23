0b7a00f76a88579868f79e20a15a3aa0eea95045
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-23 21:12:42 +0530

    File updated at 5/23/2026, 9:12:42 PM

--- 2469-ConverttheTemperature/2469-ConverttheTemperature.cpp
+++ 2469-ConverttheTemperature/2469-ConverttheTemperature.cpp
@@ -1,21 +1,7 @@
-// Last updated: 5/23/2026, 9:11:23 PM
+// Last updated: 5/23/2026, 9:12:42 PM
 1class Solution {
 2public:
-3    string destCity(vector<vector<string>>& paths) {
-4        unordered_set <string> start;
-5
-6        for(int i = 0; i < paths.size(); i++) {
-7            start.insert(paths[i][0]);
-8        }
-9
-10        for(int i = 0; i < paths.size(); i++) {
-11            string city = paths[i][1];
-12
-13            if(start.find(city) == start.end()) {
-14                return city;
-15            }
-16        }
-17
-18        return "";
-19    }
-20};
+3    int countOdds(int low, int high) {
+4        return (high + 1) / 2 - low / 2;
+5    }
+6};
