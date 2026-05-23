0f4117da91308648c858cc0b836af69c1944d138
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-23 22:29:14 +0530

    File updated at 5/23/2026, 10:29:13 PM

--- 2469-ConverttheTemperature/2469-ConverttheTemperature.cpp
+++ 2469-ConverttheTemperature/2469-ConverttheTemperature.cpp
@@ -1,14 +1,15 @@
-// Last updated: 5/23/2026, 10:27:52 PM
+// Last updated: 5/23/2026, 10:29:13 PM
 1class Solution {
 2public:
-3    int sumBase(int n, int k) {
-4        int sum = 0;
-5
-6        while(n > 0) {
-7            sum += n % k;
-8            n = n / k;
-9        }
-10
-11        return sum;
-12    }
-13};
+3    bool isPowerOfThree(int n) {
+4        if(n <= 0) {
+5            return false;
+6        }
+7
+8        while(n % 3 == 0) {
+9            n = n / 3;
+10        }
+11
+12        return n == 1;
+13    }
+14};
