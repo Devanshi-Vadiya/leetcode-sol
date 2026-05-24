50cab096fd948fdaa4864f983becc49112473c11
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-24 16:12:33 +0530

    File updated at 5/24/2026, 4:12:33 PM

--- 400-NthDigit/400-NthDigit.cpp
+++ 400-NthDigit/400-NthDigit.cpp
@@ -1,23 +1,14 @@
-// Last updated: 5/24/2026, 4:11:16 PM
+// Last updated: 5/24/2026, 4:12:33 PM
 1class Solution {
 2public:
-3    int findMaxK(vector<int>& nums) {
-4
-5        unordered_set<int> s;
-6
-7        for(int x : nums) {
-8            s.insert(x);
-9        }
-10
-11        int ans = -1;
-12
-13        for(int x : nums) {
-14
-15            if(s.find(-x) != s.end()) {
-16                ans = max(ans, abs(x));
-17            }
-18        }
-19
-20        return ans;
-21    }
-22};
+3    int findMaxK(std::vector<int>& nums) {
+4        std::sort(nums.begin(), nums.end());
+5        int n = nums.size();
+6        for (int i = n-1; i >= 0; i--) {
+7            if (nums[i] > 0 && std::binary_search(nums.begin(), nums.end(), -nums[i])) {
+8                return nums[i];
+9            }
+10        }
+11        return -1;  // If no such pair found
+12    }
+13};
