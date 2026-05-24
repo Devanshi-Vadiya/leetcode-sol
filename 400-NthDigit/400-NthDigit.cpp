f3ba6c1e0dbbd151c03b9956c6f5836b700e023d
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-24 16:23:54 +0530

    File updated at 5/24/2026, 4:23:54 PM

--- 400-NthDigit/400-NthDigit.cpp
+++ 400-NthDigit/400-NthDigit.cpp
@@ -1,14 +1,41 @@
-// Last updated: 5/24/2026, 4:12:33 PM
+// Last updated: 5/24/2026, 4:23:54 PM
 1class Solution {
 2public:
-3    int findMaxK(std::vector<int>& nums) {
-4        std::sort(nums.begin(), nums.end());
-5        int n = nums.size();
-6        for (int i = n-1; i >= 0; i--) {
-7            if (nums[i] > 0 && std::binary_search(nums.begin(), nums.end(), -nums[i])) {
-8                return nums[i];
-9            }
-10        }
-11        return -1;  // If no such pair found
-12    }
-13};
+3    int numUniqueEmails(vector<string>& emails) {
+4
+5        unordered_set<string> s;
+6
+7        for(string email : emails) {
+8
+9            string local = "";
+10            string domain = "";
+11
+12            int i = 0;
+13
+14            while(email[i] != '@') {
+15
+16                if(email[i] == '+') {
+17                    while(email[i] != '@') {
+18                        i++;
+19                    }
+20                    break;
+21                }
+22
+23                if(email[i] != '.') {
+24                    local += email[i];
+25                }
+26
+27                i++;
+28            }
+29
+30            while(i < email.length()) {
+31                domain += email[i];
+32                i++;
+33            }
+34
+35            s.insert(local + domain);
+36        }
+37
+38        return s.size();
+39    }
+40};
