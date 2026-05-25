81c2523ab661fdb026e1ee597a9520b5644c8ca5
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-25 16:41:51 +0530

    Create 929-UniqueEmailAddresses

---
+++ 929-UniqueEmailAddresses/929-UniqueEmailAddresses.cpp
@@ -0,0 +1,20 @@
+// Last updated: 5/25/2026, 4:41:52 PM
+1class Solution {
+2public:
+3    int numUniqueEmails(vector<string>& emails) {
+4
+5         unordered_set<string> st;
+6    for(string &email : emails) {
+7        string cleanEmail;
+8        for(char c : email) {
+9            if(c == '+' || c == '@') break;
+10            if(c == '.') continue;
+11            cleanEmail += c;
+12        }
+13        cleanEmail += email.substr(email.find('@'));
+14        st.insert(cleanEmail);
+15    }
+16    return st.size();
+17}   
+18    
+19};
