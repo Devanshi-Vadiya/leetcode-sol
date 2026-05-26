ff605de74bfe6f179b0532909d9e573afbc1a24c
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-05-26 13:30:33 +0530

    File updated at 5/26/2026, 1:30:33 PM

--- 929-UniqueEmailAddresses/929-UniqueEmailAddresses.cpp
+++ 929-UniqueEmailAddresses/929-UniqueEmailAddresses.cpp
@@ -1,20 +1,15 @@
-// Last updated: 5/25/2026, 4:41:52 PM
+// Last updated: 5/26/2026, 1:30:33 PM
 1class Solution {
 2public:
-3    int numUniqueEmails(vector<string>& emails) {
+3    int searchInsert(vector<int>& nums, int target) {
 4
-5         unordered_set<string> st;
-6    for(string &email : emails) {
-7        string cleanEmail;
-8        for(char c : email) {
-9            if(c == '+' || c == '@') break;
-10            if(c == '.') continue;
-11            cleanEmail += c;
-12        }
-13        cleanEmail += email.substr(email.find('@'));
-14        st.insert(cleanEmail);
-15    }
-16    return st.size();
-17}   
-18    
-19};
+5        for(int i = 0; i < nums.size(); i++) {
+6
+7            if(nums[i] >= target) {
+8                return i;
+9            }
+10        }
+11
+12        return nums.size();
+13    }
+14};
