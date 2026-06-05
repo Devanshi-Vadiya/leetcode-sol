282568bf0a72858b085dcd7eb13b3be6c426d1eb
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-05 14:44:30 +0530

    File updated at 6/5/2026, 2:44:29 PM

--- 1047-RemoveAllAdjacentDuplicatesInString/1047-RemoveAllAdjacentDuplicatesInString.cpp
+++ 1047-RemoveAllAdjacentDuplicatesInString/1047-RemoveAllAdjacentDuplicatesInString.cpp
@@ -1,26 +1,24 @@
-// Last updated: 6/5/2026, 2:29:59 PM
+// Last updated: 6/5/2026, 2:44:29 PM
 1class Solution {
 2public:
 3    string removeDuplicates(string s) {
-4        stack<char> st;
+4        stack <char> st;
 5
-6        for(char ch : s) {
-7            if(!st.empty() && st.top() == ch) {
+6        for(char ch : s){
+7            if(!st.empty() && st.top() == ch){
 8                st.pop();
-9            } else {
-10                st.push(ch);
-11            }
-12        }
-13
-14        string ans = "";
-15
-16        while(!st.empty()) {
-17            ans += st.top();
-18            st.pop();
-19        }
-20
-21        reverse(ans.begin(), ans.end());
-22
-23        return ans;
-24    }
-25};
+9            }
+10            else{
+11                st.push(ch);
+12            }
+13        }
+14
+15        string ans = "";
+16
+17        while(!st.empty()){
+18            ans = st.top() + ans;
+19            st.pop();
+20        }
+21        return ans;
+22    }
+23};
