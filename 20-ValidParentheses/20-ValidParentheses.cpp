220670dc8d186a726f0c274674d9ece442b5626d
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-07 22:30:06 +0530

    File updated at 6/7/2026, 10:30:05 PM

--- 20-ValidParentheses/20-ValidParentheses.cpp
+++ 20-ValidParentheses/20-ValidParentheses.cpp
@@ -1,35 +1,36 @@
-// Last updated: 6/6/2026, 1:41:16 PM
+// Last updated: 6/7/2026, 10:30:05 PM
 1class Solution {
 2public:
 3    bool isValid(string s) {
-4        stack<char> st;
-5
-6        for(char ch : s) {
-7
-8            if(ch == '(' || ch == '{' || ch == '[') {
-9                st.push(ch);
-10            }
-11            else {
+4
+5            stack <char> st;
+6
+7            for(char ch : s){
+8                if(ch=='{' || ch == '(' || ch == '['){
+9                    st.push(ch);
+10                }
+11
 12
-13                if(st.empty()) {
-14                    return false;
-15                }
-16
-17                if(ch == ')' && st.top() == '(') {
-18                    st.pop();
-19                }
-20                else if(ch == '}' && st.top() == '{') {
-21                    st.pop();
-22                }
-23                else if(ch == ']' && st.top() == '[') {
-24                    st.pop();
-25                }
-26                else {
-27                    return false;
-28                }
-29            }
-30        }
-31
-32        return st.empty();
-33    }
-34};
+13                else{
+14                    if(st.empty()) {
+15                        return false;
+16                    }
+17
+18                    if(ch == ')' && st.top() =='('){
+19                        st.pop();
+20                    }
+21                   else if(ch == '}' && st.top() =='{'){
+22                        st.pop();
+23                    }
+24                   else if(ch == ']' && st.top() =='['){
+25                        st.pop();
+26                    }
+27                    else{
+28                        return false;
+29                    }
+30
+31                }
+32            }
+33            return st.empty();
+34    }
+35};
