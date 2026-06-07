fbb8e216f6927c307df7f33e1707f4963f328bbb
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-07 22:43:21 +0530

    Create 326-PowerofThree

---
+++ 326-PowerofThree/326-PowerofThree.cpp
@@ -0,0 +1,13 @@
+// Last updated: 6/7/2026, 10:43:20 PM
+1class Solution {
+2public:
+3    bool isPowerOfFour(int n) {
+4        if(n<=0){
+5            return false;
+6        }
+7        while(n%4 ==0){
+8            n/=4;
+9        }
+10        return n == 1;
+11    }
+12};
