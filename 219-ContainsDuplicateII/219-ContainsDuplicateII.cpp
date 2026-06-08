2ed0f013f8cfa2db5b67a8b23199b724f2f8af00
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-08 10:32:32 +0530

    File updated at 6/8/2026, 10:32:31 AM

--- 219-ContainsDuplicateII/219-ContainsDuplicateII.cpp
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.cpp
@@ -1,18 +1,19 @@
-// Last updated: 6/8/2026, 10:14:08 AM
+// Last updated: 6/8/2026, 10:32:31 AM
 1class Solution {
 2public:
 3    bool containsNearbyDuplicate(vector<int>& nums, int k) {
 4
-5        for(int i = 0; i < nums.size(); i++) {
+5        unordered_map<int,int> mp;
 6
-7            for(int j = i + 1; j < nums.size() && j - i <= k; j++) {
+7        for(int i = 0; i < nums.size(); i++) {
 8
-9                if(nums[i] == nums[j]) {
-10                    return true;
-11                }
-12            }
-13        }
-14
-15        return false;
-16    }
-17};
+9            if(mp.count(nums[i]) && i - mp[nums[i]] <= k) {//stores the number and the last index where it is stored. and check the condition of k.
+10                return true; 
+11            }
+12
+13            mp[nums[i]] = i;
+14        }
+15
+16        return false;
+17    }
+18};
