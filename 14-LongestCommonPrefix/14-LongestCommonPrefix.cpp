2eb9c3e3096e0d56404f4aae7096ea8b9697e1b7
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-14 17:32:56 +0530

    File updated at 6/14/2026, 5:32:56 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
@@ -1,19 +1,21 @@
-// Last updated: 4/24/2026, 5:06:44 PM
-class Solution {
-public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string prefix = strs[0];
-
-        for (int i = 1; i < strs.size(); i++) {
-            // Keep removing last character until prefix matches
-            while (strs[i].find(prefix) != 0) {
-                prefix.pop_back();
-                
-                if (prefix.empty())
-                    return "";
-            }
-        }
-
-        return prefix;
-    }
-};
+// Last updated: 6/14/2026, 5:32:56 PM
+1class Solution {
+2public:
+3    int maxProfit(vector<int>& prices) {
+4        int minPrice = prices[0];
+5        int maxProfit = 0;
+6
+7        for(int i =1;i<prices.size();i++){
+8            if(prices[i]<minPrice){
+9                minPrice = prices [i];
+10            }
+11
+12            int profit = prices[i] - minPrice;
+13
+14            if(profit > maxProfit){
+15                maxProfit = profit;
+16            }
+17        }
+18        return maxProfit;
+19    }
+20};
