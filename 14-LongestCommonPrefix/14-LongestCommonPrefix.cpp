fb763ec72c101757a025b206fcf6f646e5a34a52
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-14 17:35:30 +0530

    File updated at 6/14/2026, 5:35:29 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
@@ -1,21 +1,18 @@
-// Last updated: 6/14/2026, 5:32:56 PM
+// Last updated: 6/14/2026, 5:35:29 PM
 1class Solution {
 2public:
-3    int maxProfit(vector<int>& prices) {
-4        int minPrice = prices[0];
-5        int maxProfit = 0;
+3    int majorityElement(vector<int>& nums) {
+4        unordered_map<int, int> mp;
+5        int n = nums.size();
 6
-7        for(int i =1;i<prices.size();i++){
-8            if(prices[i]<minPrice){
-9                minPrice = prices [i];
-10            }
-11
-12            int profit = prices[i] - minPrice;
-13
-14            if(profit > maxProfit){
-15                maxProfit = profit;
-16            }
-17        }
-18        return maxProfit;
-19    }
-20};
+7        for (int num : nums) {
+8            mp[num]++;
+9
+10            if (mp[num] > n / 2) {
+11                return num;
+12            }
+13        }
+14
+15        return -1;
+16    }
+17};
