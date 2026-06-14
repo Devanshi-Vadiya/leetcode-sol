b39de6325471d8f14509613378105794ca7457f6
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-14 17:59:51 +0530

    File updated at 6/14/2026, 5:59:50 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
@@ -1,20 +1,19 @@
-// Last updated: 6/14/2026, 5:56:20 PM
+// Last updated: 6/14/2026, 5:59:50 PM
 1class Solution {
 2public:
-3    bool isUgly(int n) {
-4        
-5        if(n<=0){
-6            return false;
-7        }
-8        while(n%2 ==0){
-9            n/=2;
-10        }
-11        while(n%3 ==0){
-12            n/=3;
-13        }
-14        while(n%5 ==0){
-15            n/=5;
-16        }
-17        return n == 1;
-18    }
-19};
+3    int findMaxConsecutiveOnes(vector<int>& nums) {
+4        int count = 0;
+5        int ans = 0;
+6
+7        for(int num : nums){
+8            if(num == 1){
+9                count++;
+10            }
+11            else{
+12                count = 0;
+13            }
+14            ans = max(ans,count);
+15        }
+16        return ans;
+17    }
+18};
