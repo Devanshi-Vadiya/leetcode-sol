51c1e9cb799abd10456659e5b62e4df4436f9617
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-16 11:47:04 +0530

    File updated at 6/16/2026, 11:47:05 AM

--- 169-MajorityElement/169-MajorityElement.cpp
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -1,26 +1,23 @@
-// Last updated: 4/24/2026, 5:06:29 PM
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) 
-    {
-        //frequency count of individual elemnts:
-        unordered_map<int,int>mp;
-
-        //iteration of given vector:
-        for(int value : nums){
-            mp[value]++; //key exist increment cureent value ; if doesnt exist . create it and increment to 1
-        }
-
-        int freq = nums.size()/2;
-        //find that element > freq
-        int val;
-        for(auto& it : mp){
-            if(it.second>freq)
-                  {
-                    val = it.first;
-                    break;
-                  }
-        }
-        return val;
-    }
-};
+// Last updated: 6/16/2026, 11:47:05 AM
+1class Solution {
+2public:
+3    int majorityElement(vector<int>& nums) {
+4        int candidate = 0;
+5        int count = 0;
+6
+7        for (int num : nums) {
+8
+9            if (count == 0) {
+10                candidate = num;
+11            }
+12
+13            if (num == candidate) {
+14                count++;
+15            } else {
+16                count--;
+17            }
+18        }
+19
+20        return candidate;
+21    }
+22};
