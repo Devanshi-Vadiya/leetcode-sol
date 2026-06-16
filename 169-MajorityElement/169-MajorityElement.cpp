4590a12713747968b4f22582b4cae47b9a1a6000
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-16 13:28:11 +0530

    File updated at 6/16/2026, 1:28:12 PM

--- 169-MajorityElement/169-MajorityElement.cpp
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -1,23 +1,39 @@
-// Last updated: 6/16/2026, 11:47:05 AM
+// Last updated: 6/16/2026, 1:28:12 PM
 1class Solution {
 2public:
-3    int majorityElement(vector<int>& nums) {
-4        int candidate = 0;
-5        int count = 0;
-6
-7        for (int num : nums) {
+3    int compareVersion(string version1, string version2) {
+4
+5        int i = 0, j = 0;
+6        int n = version1.size();
+7        int m = version2.size();
 8
-9            if (count == 0) {
-10                candidate = num;
-11            }
-12
-13            if (num == candidate) {
-14                count++;
-15            } else {
-16                count--;
+9        while (i < n || j < m) {
+10
+11            int num1 = 0;
+12            int num2 = 0;
+13
+14            while (i < n && version1[i] != '.') {
+15                num1 = num1 * 10 + (version1[i] - '0');
+16                i++;
 17            }
-18        }
-19
-20        return candidate;
-21    }
-22};
+18
+19            while (j < m && version2[j] != '.') {
+20                num2 = num2 * 10 + (version2[j] - '0');
+21                j++;
+22            }
+23
+24            if (num1 > num2) {
+25                return 1;
+26            }
+27
+28            if (num1 < num2) {
+29                return -1;
+30            }
+31
+32            i++;
+33            j++;
+34        }
+35
+36        return 0;
+37    }
+38};
