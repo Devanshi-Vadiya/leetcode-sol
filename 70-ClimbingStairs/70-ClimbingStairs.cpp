0ddf234fc9fec4ad4337634a6dc0724aeb48ea7c
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-18 13:33:44 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,26 @@
+// Last updated: 6/18/2026, 1:33:44 PM
+1class Solution {
+2public:
+3    int climbStairs(int n) {
+4         //1 = 1 
+5        //2 = 2
+6        //3 = 3
+7        //4 = 5
+8        //5 = 8 
+9
+10        if(n<=2){
+11            return n;
+12        }
+13
+14        int first = 1;
+15        int second = 2;
+16
+17
+18        for(int i = 3;i<=n;i++){
+19            int current = first + second;
+20            first = second;
+21            second = current ;
+22        }
+23        return second;
+24    }
+25};
