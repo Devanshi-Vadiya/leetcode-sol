5f58d0f4e5c5e50e3a04ab73cadfbafc75df3c01
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-24 12:27:46 +0530

    File updated at 6/24/2026, 12:27:49 PM

--- 1221-SplitaStringinBalancedStrings/1221-SplitaStringinBalancedStrings.cpp
+++ 1221-SplitaStringinBalancedStrings/1221-SplitaStringinBalancedStrings.cpp
@@ -1,7 +1,22 @@
-// Last updated: 6/17/2026, 11:36:51 AM
+// Last updated: 6/24/2026, 12:27:49 PM
 1class Solution {
 2public:
-3    int numberOfMatches(int n) {
-4        return (n-1);
-5    }
-6};
+3    int balancedStringSplit(string s) {
+4     int count = 0;
+5     int balance = 0;
+6
+7     for(char ch :s ){
+8        if(ch == 'L'){
+9            balance ++;
+10        }
+11        else{
+12            balance --;
+13        }
+14
+15        if(balance == 0){
+16            count ++;
+17        }
+18     }  
+19     return count; 
+20    }
+21};
