7f39f7a091cea1b1a0727bc64885e92db4f46fe1
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-25 09:04:08 +0530

    File updated at 6/25/2026, 9:04:07 AM

--- 3945-DigitFrequencyScore/3945-DigitFrequencyScore.cpp
+++ 3945-DigitFrequencyScore/3945-DigitFrequencyScore.cpp
@@ -1,14 +1,13 @@
-// Last updated: 6/13/2026, 11:50:13 AM
+// Last updated: 6/25/2026, 9:04:07 AM
 1class Solution {
 2public:
 3    int digitFrequencyScore(int n) {
 4        int ans = 0;
 5
-6        while (n > 0) {
-7            ans += n % 10;
-8            n /= 10;
+6        while(n>0){
+7            ans+=n%10;
+8            n/=10;
 9        }
-10
-11        return ans;
-12    }
-13};
+10        return ans;
+11    }
+12};
