7c26855ae9017059025c8a605c5f6bda39068185
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-29 10:53:49 +0530

    File updated at 6/29/2026, 10:53:48 AM

--- 69-Sqrt(x)/69-Sqrt(x).cpp
+++ 69-Sqrt(x)/69-Sqrt(x).cpp
@@ -1,2 +1,26 @@
-// Last updated: 6/29/2026, 10:53:24 AM
-int len=(n<=26)?1:ceil(log(n*25.0/26+1)/log(26));
+// Last updated: 6/29/2026, 10:53:48 AM
+1class Solution {
+2public:
+3    int squareSum(int n) {
+4        int sum = 0;
+5
+6        while (n > 0) {
+7            int digit = n % 10;
+8            sum += digit * digit;
+9            n /= 10;
+10        }
+11
+12        return sum;
+13    }
+14
+15    bool isHappy(int n) {
+16        unordered_set<int> visited;
+17
+18        while (n != 1 && !visited.count(n)) {
+19            visited.insert(n);
+20            n = squareSum(n);
+21        }
+22
+23        return n == 1;
+24    }
+25};
