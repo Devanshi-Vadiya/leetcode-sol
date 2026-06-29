caacc6fd269b66342db463d09c6796f9791aa7a3
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-29 10:40:00 +0530

    File updated at 6/29/2026, 10:40:00 AM

--- 2225-FindPlayersWithZeroorOneLosses/2225-FindPlayersWithZeroorOneLosses.cpp
+++ 2225-FindPlayersWithZeroorOneLosses/2225-FindPlayersWithZeroorOneLosses.cpp
@@ -1,35 +1,16 @@
-// Last updated: 6/29/2026, 10:39:21 AM
+// Last updated: 6/29/2026, 10:40:00 AM
 1class Solution {
 2public:
-3    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-4        
-5        map<int, int> lossCount;
+3    int trailingZeroes(int n) {
+4
+5        int count = 0;
 6
-7        for(auto match : matches) {
-8            int winner = match[0];
-9            int loser = match[1];
-10
-11            // ensure winner exists in map
-12            if(lossCount.find(winner) == lossCount.end()) {
-13                lossCount[winner] = 0;
-14            }
-15
-16            // increase loser count
-17            lossCount[loser]++;
-18        }
-19
-20        vector<int> zeroLoss;
-21        vector<int> oneLoss;
-22
-23        for(auto player : lossCount) {
-24            if(player.second == 0) {
-25                zeroLoss.push_back(player.first);
-26            }
-27            else if(player.second == 1) {
-28                oneLoss.push_back(player.first);
-29            }
-30        }
-31
-32        return {zeroLoss, oneLoss};
-33    }
-34};
+7        while(n > 0){
+8
+9            n = n / 5;
+10            count = count + n;
+11        }
+12
+13        return count;
+14    }
+15};
