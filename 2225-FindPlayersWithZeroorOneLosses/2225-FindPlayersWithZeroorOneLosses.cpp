49fcd797e61fcb0b57d81688584403e7b67b3bd6
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-29 10:40:32 +0530

    File updated at 6/29/2026, 10:40:31 AM

--- 2225-FindPlayersWithZeroorOneLosses/2225-FindPlayersWithZeroorOneLosses.cpp
+++ 2225-FindPlayersWithZeroorOneLosses/2225-FindPlayersWithZeroorOneLosses.cpp
@@ -1,16 +1,14 @@
-// Last updated: 6/29/2026, 10:40:00 AM
+// Last updated: 6/29/2026, 10:40:31 AM
 1class Solution {
 2public:
-3    int trailingZeroes(int n) {
+3    vector<long long> sumOfThree(long long num) {
 4
-5        int count = 0;
-6
-7        while(n > 0){
+5        if(num % 3 != 0){
+6            return {};
+7        }
 8
-9            n = n / 5;
-10            count = count + n;
-11        }
-12
-13        return count;
-14    }
-15};
+9        long long x = num / 3;
+10
+11        return {x - 1, x, x + 1};
+12    }
+13};
