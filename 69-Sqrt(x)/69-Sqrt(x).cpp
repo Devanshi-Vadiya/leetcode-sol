571f3e8a56d49574562db4d7123499822b33562d
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-29 10:56:55 +0530

    File updated at 6/29/2026, 10:56:54 AM

--- 69-Sqrt(x)/69-Sqrt(x).cpp
+++ 69-Sqrt(x)/69-Sqrt(x).cpp
@@ -1,8 +1,22 @@
-// Last updated: 6/29/2026, 10:55:58 AM
+// Last updated: 6/29/2026, 10:56:54 AM
 1class Solution {
 2public:
-3    int distributeCandies(vector<int>& candyType) {
-4        unordered_set<int> s(candyType.begin(), candyType.end());
-5        return min((int)s.size(), (int)candyType.size() / 2);
-6    }
-7};
+3    vector<int> sortArrayByParityII(vector<int>& nums) {
+4        int n = nums.size();
+5        vector<int> ans(n);
+6
+7        int even = 0, odd = 1;
+8
+9        for (int num : nums) {
+10            if (num % 2 == 0) {
+11                ans[even] = num;
+12                even += 2;
+13            } else {
+14                ans[odd] = num;
+15                odd += 2;
+16            }
+17        }
+18
+19        return ans;
+20    }
+21};
