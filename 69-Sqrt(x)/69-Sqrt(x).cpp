b7923ebe13d89a8c01c0e030f6ea7e1fe393a265
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-29 10:53:24 +0530

    File updated at 6/29/2026, 10:53:24 AM

--- 69-Sqrt(x)/69-Sqrt(x).cpp
+++ 69-Sqrt(x)/69-Sqrt(x).cpp
@@ -1,20 +1,2 @@
-// Last updated: 6/29/2026, 10:53:03 AM
-1class Solution {
-2public:
-3    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-4        int i = m - 1;
-5        int j = n - 1;
-6        int k = m + n - 1;
-7
-8        while (i >= 0 && j >= 0) {
-9            if (nums1[i] > nums2[j])
-10                nums1[k--] = nums1[i--];
-11            else
-12                nums1[k--] = nums2[j--];
-13        }
-14
-15        while (j >= 0) {
-16            nums1[k--] = nums2[j--];
-17        }
-18    }
-19};
+// Last updated: 6/29/2026, 10:53:24 AM
+int len=(n<=26)?1:ceil(log(n*25.0/26+1)/log(26));
