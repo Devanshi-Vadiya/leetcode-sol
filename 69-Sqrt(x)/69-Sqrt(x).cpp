95d7da85b6d28760b4f00865fab85549979624e2
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-29 10:57:18 +0530

    File updated at 6/29/2026, 10:57:17 AM

--- 69-Sqrt(x)/69-Sqrt(x).cpp
+++ 69-Sqrt(x)/69-Sqrt(x).cpp
@@ -1,22 +1,31 @@
-// Last updated: 6/29/2026, 10:56:54 AM
+// Last updated: 6/29/2026, 10:57:17 AM
 1class Solution {
 2public:
-3    vector<int> sortArrayByParityII(vector<int>& nums) {
-4        int n = nums.size();
-5        vector<int> ans(n);
-6
-7        int even = 0, odd = 1;
+3    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+4        unordered_map<int, int> freq;
+5
+6        for (int num : arr1)
+7            freq[num]++;
 8
-9        for (int num : nums) {
-10            if (num % 2 == 0) {
-11                ans[even] = num;
-12                even += 2;
-13            } else {
-14                ans[odd] = num;
-15                odd += 2;
-16            }
-17        }
+9        vector<int> ans;
+10
+11        for (int num : arr2) {
+12            while (freq[num]--) {
+13                ans.push_back(num);
+14            }
+15        }
+16
+17        vector<int> rem;
 18
-19        return ans;
-20    }
-21};
+19        for (auto &p : freq) {
+20            while (p.second-- > 0)
+21                rem.push_back(p.first);
+22        }
+23
+24        sort(rem.begin(), rem.end());
+25
+26        ans.insert(ans.end(), rem.begin(), rem.end());
+27
+28        return ans;
+29    }
+30};
