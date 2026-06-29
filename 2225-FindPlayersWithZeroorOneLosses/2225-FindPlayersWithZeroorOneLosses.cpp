10aaa2c55f23be78d62dcff68b51d68780828751
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-29 10:42:13 +0530

    File updated at 6/29/2026, 10:42:13 AM

--- 2225-FindPlayersWithZeroorOneLosses/2225-FindPlayersWithZeroorOneLosses.cpp
+++ 2225-FindPlayersWithZeroorOneLosses/2225-FindPlayersWithZeroorOneLosses.cpp
@@ -1,29 +1,8 @@
-// Last updated: 6/29/2026, 10:41:08 AM
+// Last updated: 6/29/2026, 10:42:13 AM
 1class Solution {
 2public:
-3    bool sumOfNumberAndReverse(int num) {
-4
-5        for(int i = 0; i <= num; i++) {
-6
-7            int sum = 0;
-8            int x = i;
-9
-10            // reverse x
-11            while(x > 0) {
-12
-13                int rem = x % 10;
-14
-15                sum = sum * 10 + rem;
-16
-17                x = x / 10;
-18            }
-19
-20            // check condition
-21            if(i + sum == num) {
-22                return true;
-23            }
-24        }
-25
-26        return false;
-27    }
-28};
+3    vector<int> sortArray(vector<int>& nums) {
+4        sort(nums.begin(), nums.end());
+5        return nums;
+6    }
+7};
