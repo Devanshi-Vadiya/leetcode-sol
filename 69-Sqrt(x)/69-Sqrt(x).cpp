a17d13bbebba3e2170adec225773377a335b4251
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-06-29 10:55:58 +0530

    File updated at 6/29/2026, 10:55:58 AM

--- 69-Sqrt(x)/69-Sqrt(x).cpp
+++ 69-Sqrt(x)/69-Sqrt(x).cpp
@@ -1,20 +1,8 @@
-// Last updated: 6/29/2026, 10:55:32 AM
+// Last updated: 6/29/2026, 10:55:58 AM
 1class Solution {
 2public:
-3    vector<string> fizzBuzz(int n) {
-4        vector<string> ans;
-5
-6        for (int i = 1; i <= n; i++) {
-7            if (i % 15 == 0)
-8                ans.push_back("FizzBuzz");
-9            else if (i % 3 == 0)
-10                ans.push_back("Fizz");
-11            else if (i % 5 == 0)
-12                ans.push_back("Buzz");
-13            else
-14                ans.push_back(to_string(i));
-15        }
-16
-17        return ans;
-18    }
-19};
+3    int distributeCandies(vector<int>& candyType) {
+4        unordered_set<int> s(candyType.begin(), candyType.end());
+5        return min((int)s.size(), (int)candyType.size() / 2);
+6    }
+7};
