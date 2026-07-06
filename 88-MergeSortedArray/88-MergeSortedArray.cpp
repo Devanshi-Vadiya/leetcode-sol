908d6ccb1b293704af695ef0e692d7abbe2a4009
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-07-06 12:03:36 +0530

    File updated at 7/6/2026, 12:03:37 PM

--- 88-MergeSortedArray/88-MergeSortedArray.cpp
+++ 88-MergeSortedArray/88-MergeSortedArray.cpp
@@ -1,20 +1,20 @@
-// Last updated: 5/19/2026, 3:32:39 PM
+// Last updated: 7/6/2026, 12:03:37 PM
 1class Solution {
 2public:
-3    int numJewelsInStones(string jewels, string stones) {
-4        unordered_set<char> jewelSet;
-5
-6        for(int i =0;i<jewels.size();i++){
-7            jewelSet.insert(jewels[i]);
-8        }
-9
-10        int count = 0;
-11
-12        for(int i =0;i<stones.size();i++){
-13            if(jewelSet.count(stones[i])){
-14                count++;
-15            }
-16        }
-17        return count;
+3    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+4        int i = m - 1;
+5        int j = n - 1;
+6        int k = m + n - 1;
+7
+8        while (i >= 0 && j >= 0) {
+9            if (nums1[i] > nums2[j])
+10                nums1[k--] = nums1[i--];
+11            else
+12                nums1[k--] = nums2[j--];
+13        }
+14
+15        while (j >= 0) {
+16            nums1[k--] = nums2[j--];
+17        }
 18    }
 19};
