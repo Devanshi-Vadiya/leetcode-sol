7d4fd1c1efdc87e8969e228a3c77e3ae01ca70a7
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-07-08 14:43:00 +0530

    File updated at 7/8/2026, 2:43:00 PM

--- 1450-NumberofStudentsDoingHomeworkataGivenTime/1450-NumberofStudentsDoingHomeworkataGivenTime.cpp
+++ 1450-NumberofStudentsDoingHomeworkataGivenTime/1450-NumberofStudentsDoingHomeworkataGivenTime.cpp
@@ -1,14 +1,15 @@
-// Last updated: 7/8/2026, 2:41:46 PM
+// Last updated: 7/8/2026, 2:43:00 PM
 1class Solution {
 2public:
-3    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
-4        int count = 0;
+3    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+4        string s1 = "", s2 = "";
 5
-6        for (int i = 0; i < startTime.size(); i++) {
-7            if (startTime[i] <= queryTime && queryTime <= endTime[i])
-8                count++;
-9        }
-10
-11        return count;
-12    }
-13};
+6        for (string s : word1)
+7            s1 += s;
+8
+9        for (string s : word2)
+10            s2 += s;
+11
+12        return s1 == s2;
+13    }
+14};
