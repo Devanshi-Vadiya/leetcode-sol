128d0f5dcc4010a086423cf61619076a906c016b
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-07-08 15:05:20 +0530

    File updated at 7/8/2026, 3:05:20 PM

--- 2215-FindtheDifferenceofTwoArrays/2215-FindtheDifferenceofTwoArrays.cpp
+++ 2215-FindtheDifferenceofTwoArrays/2215-FindtheDifferenceofTwoArrays.cpp
@@ -1,22 +1,23 @@
-// Last updated: 7/8/2026, 3:02:33 PM
+// Last updated: 7/8/2026, 3:05:20 PM
 1class Solution {
 2public:
-3    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-4        unordered_set<int> s1(nums1.begin(), nums1.end());
-5        unordered_set<int> s2(nums2.begin(), nums2.end());
-6
-7        vector<int> a, b;
+3    vector<int> intersection(vector<vector<int>>& nums) {
+4        map<int, int> mp;
+5
+6        for (auto &v : nums) {
+7            unordered_set<int> st(v.begin(), v.end());
 8
-9        for (int x : s1) {
-10            if (!s2.count(x))
-11                a.push_back(x);
-12        }
-13
-14        for (int x : s2) {
-15            if (!s1.count(x))
-16                b.push_back(x);
-17        }
-18
-19        return {a, b};
-20    }
-21};
+9            for (int x : st)
+10                mp[x]++;
+11        }
+12
+13        vector<int> ans;
+14
+15        for (auto it : mp) {
+16            if (it.second == nums.size())
+17                ans.push_back(it.first);
+18        }
+19
+20        return ans;
+21    }
+22};
