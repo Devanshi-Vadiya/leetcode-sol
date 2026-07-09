b36ce129d4f9c93a5adc2142b52faa053318d0f8
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-07-09 14:34:23 +0530

    File updated at 7/9/2026, 2:34:24 PM

--- 2363-MergeSimilarItems/2363-MergeSimilarItems.cpp
+++ 2363-MergeSimilarItems/2363-MergeSimilarItems.cpp
@@ -1,14 +1,24 @@
-// Last updated: 7/9/2026, 2:33:59 PM
+// Last updated: 7/9/2026, 2:34:24 PM
 1class Solution {
 2public:
-3    int buyChoco(vector<int>& prices, int money) {
-4        sort(prices.begin(), prices.end());
+3    int distanceTraveled(int mainTank, int additionalTank) {
+4        int distance = 0;
 5
-6        int cost = prices[0] + prices[1];
-7
-8        if (cost <= money)
-9            return money - cost;
+6        while (mainTank > 0) {
+7            if (mainTank >= 5) {
+8                distance += 50;
+9                mainTank -= 5;
 10
-11        return money;
-12    }
-13};
+11                if (additionalTank > 0) {
+12                    mainTank++;
+13                    additionalTank--;
+14                }
+15            } else {
+16                distance += mainTank * 10;
+17                break;
+18            }
+19        }
+20
+21        return distance;
+22    }
+23};
