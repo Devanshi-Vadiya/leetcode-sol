291f314348ea32405d8c79767a07048dfa895572
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-07-09 14:35:05 +0530

    File updated at 7/9/2026, 2:35:07 PM

--- 2363-MergeSimilarItems/2363-MergeSimilarItems.cpp
+++ 2363-MergeSimilarItems/2363-MergeSimilarItems.cpp
@@ -1,24 +1,24 @@
-// Last updated: 7/9/2026, 2:34:24 PM
+// Last updated: 7/9/2026, 2:35:07 PM
 1class Solution {
 2public:
-3    int distanceTraveled(int mainTank, int additionalTank) {
-4        int distance = 0;
+3    int maxFrequencyElements(vector<int>& nums) {
+4        unordered_map<int, int> mp;
 5
-6        while (mainTank > 0) {
-7            if (mainTank >= 5) {
-8                distance += 50;
-9                mainTank -= 5;
+6        for (int num : nums)
+7            mp[num]++;
+8
+9        int maxFreq = 0;
 10
-11                if (additionalTank > 0) {
-12                    mainTank++;
-13                    additionalTank--;
-14                }
-15            } else {
-16                distance += mainTank * 10;
-17                break;
-18            }
+11        for (auto it : mp)
+12            maxFreq = max(maxFreq, it.second);
+13
+14        int ans = 0;
+15
+16        for (auto it : mp) {
+17            if (it.second == maxFreq)
+18                ans += it.second;
 19        }
 20
-21        return distance;
+21        return ans;
 22    }
 23};
