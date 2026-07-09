5deaa8451a01e355cf5c326f1321bfbfc7e3036b
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-07-09 14:35:36 +0530

    File updated at 7/9/2026, 2:35:37 PM

--- 2363-MergeSimilarItems/2363-MergeSimilarItems.cpp
+++ 2363-MergeSimilarItems/2363-MergeSimilarItems.cpp
@@ -1,24 +1,14 @@
-// Last updated: 7/9/2026, 2:35:07 PM
+// Last updated: 7/9/2026, 2:35:37 PM
 1class Solution {
 2public:
-3    int maxFrequencyElements(vector<int>& nums) {
-4        unordered_map<int, int> mp;
+3    int countKeyChanges(string s) {
+4        int count = 0;
 5
-6        for (int num : nums)
-7            mp[num]++;
-8
-9        int maxFreq = 0;
+6        for (int i = 1; i < s.size(); i++) {
+7            if (tolower(s[i]) != tolower(s[i - 1]))
+8                count++;
+9        }
 10
-11        for (auto it : mp)
-12            maxFreq = max(maxFreq, it.second);
-13
-14        int ans = 0;
-15
-16        for (auto it : mp) {
-17            if (it.second == maxFreq)
-18                ans += it.second;
-19        }
-20
-21        return ans;
-22    }
-23};
+11        return count;
+12    }
+13};
