8dce717c2cf0014d903fe48cb667c0584f35df03
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-07-09 09:14:18 +0530

    File updated at 7/9/2026, 9:14:19 AM

--- 7-ReverseInteger/7-ReverseInteger.cpp
+++ 7-ReverseInteger/7-ReverseInteger.cpp
@@ -1,21 +1,18 @@
-// Last updated: 4/24/2026, 5:06:47 PM
-class Solution {
-public:
-    int reverse(int x) {
-        int rev = 0;
-        
-        while (x != 0) {
-            int digit = x % 10;
-            x /= 10;
-
-            // Check for overflow before multiplying
-            if (rev > INT_MAX / 10 || rev < INT_MIN / 10) {
-                return 0;
-            }
-
-            rev = rev * 10 + digit;
-        }
-
-        return rev;
-    }
-};
+// Last updated: 7/9/2026, 9:14:19 AM
+1class Solution {
+2public:
+3    int reverse(int x) {
+4        int ans = 0, rem;
+5
+6        while(x){
+7
+8            rem = x%10;
+9            if(ans>INT_MAX/10  || ans< INT_MIN/10  )
+10           { return 0;
+11           }
+12            ans=ans*10+rem;
+13            x/=10;
+14        }
+15        return ans;
+16    }
+17};
