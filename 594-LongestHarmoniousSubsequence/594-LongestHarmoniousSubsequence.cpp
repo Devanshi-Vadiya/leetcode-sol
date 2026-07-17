4f6ec6c1c17db58da98d37e37cfd0c3482f9cbe1
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-07-17 13:16:05 +0530

    Create 594-LongestHarmoniousSubsequence

---
+++ 594-LongestHarmoniousSubsequence/594-LongestHarmoniousSubsequence.cpp
@@ -0,0 +1,22 @@
+// Last updated: 7/17/2026, 1:16:08 PM
+1class Solution {
+2public:
+3    int findLHS(vector<int>& nums) {
+4        unordered_map<int, int> mp;
+5        int ans = 0;
+6
+7        for (int num : nums) {
+8            mp[num]++;
+9        }
+10
+11        for (auto it : mp) {
+12            int num = it.first;
+13
+14            if (mp.count(num + 1)) {
+15                ans = max(ans, mp[num] + mp[num + 1]);
+16            }
+17        }
+18
+19        return ans;
+20    }
+21};
