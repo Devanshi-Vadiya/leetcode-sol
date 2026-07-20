6c65d8463c292f5d6bcd1025e673e57489ca7509
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-07-20 09:13:17 +0530

    File updated at 7/20/2026, 9:13:18 AM

--- 49-GroupAnagrams/49-GroupAnagrams.cpp
+++ 49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -1,26 +1,27 @@
-// Last updated: 4/24/2026, 5:06:38 PM
-class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
-
-        for (string s : strs) {
-            string temp = s;
-            sort(temp.begin(), temp.end());
-            mp[temp].push_back(s);
-        }
-        //for(string word: nums){
-        //     string=word;
-        //     sort(temp.begin(),temp.end());
-        //      anagram[temp].push_back(word);
-        // }
-
-        vector<vector<string>> ans;
-
-        for (auto & x : mp) {
-            ans.push_back(x.second);
-        }
-        //for(auto )
-        return ans;
-    }
-};
+// Last updated: 7/20/2026, 9:13:18 AM
+1class Solution {
+2public:
+3    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+4        unordered_map<string, vector<string>> mp;
+5
+6        for (string s : strs) {
+7            string temp = s;
+8            sort(temp.begin(), temp.end());
+9            mp[temp].push_back(s);
+10        }
+11        //for(string word: nums){
+12        //     string=word;
+13        //     sort(temp.begin(),temp.end());
+14        //      anagram[temp].push_back(word);
+15        // }
+16
+17        vector<vector<string>> ans;
+18
+19        for (auto & x : mp) {
+20            ans.push_back(x.second);
+21        }
+22        //for(auto )
+23        return ans;
+24    }
+25};
+26
