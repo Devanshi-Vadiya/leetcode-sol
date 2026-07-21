23d1e0db098a15654b5d9515efb2c29f34495f34
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-07-21 14:04:29 +0530

    File updated at 7/21/2026, 2:04:31 PM

--- 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.cpp
+++ 1456-MaximumNumberofVowelsinaSubstringofGivenLength/1456-MaximumNumberofVowelsinaSubstringofGivenLength.cpp
@@ -1,34 +1,23 @@
-// Last updated: 7/21/2026, 1:39:37 PM
+// Last updated: 7/21/2026, 2:04:31 PM
 1class Solution {
 2public:
-3    bool isVowel(char ch) {
-4        return ch == 'a' || ch == 'e' || ch == 'i' ||
-5               ch == 'o' || ch == 'u';
-6    }
-7
-8    int maxVowels(string s, int k) {
-9        int count = 0;
-10        int ans = 0;
-11
-12        for (int i = 0; i < k; i++) {
-13            if (isVowel(s[i]))
-14                count++;
-15        }
-16
-17        ans = count;
-18
-19        for (int i = k; i < s.size(); i++) {
-20            if (isVowel(s[i]))
-21                count++;
-22
-23            if (isVowel(s[i - k]))
-24                count--;
-25
-26            ans = max(ans, count);
-27        }
-28
-29        return ans;
-30    }
-31};
-32
-33
+3    vector<int> findAnagrams(string s, string p) {
+4        vector<int> res;
+5        if (p.length() > s.length())
+6            return res;
+7        vector<int> p_count(26, 0), w_count(26, 0);
+8        for (int i = 0; i < p.length(); ++i) {
+9            p_count[p[i] - 'a']++;
+10            w_count[s[i] - 'a']++;
+11        }
+12        if (p_count == w_count)
+13            res.push_back(0);
+14        for (int i = p.length(); i < s.length(); ++i) {
+15            w_count[s[i] - 'a']++;
+16            w_count[s[i - p.length()] - 'a']--;
+17            if (p_count == w_count)
+18                res.push_back(i - p.length() + 1);
+19        }
+20        return res;
+21    }
+22};
