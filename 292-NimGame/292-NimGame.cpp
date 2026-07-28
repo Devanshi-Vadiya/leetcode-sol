70e70664b7df3f64b4661d58e4e42ed26044518f
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-07-28 13:34:02 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.cpp
@@ -0,0 +1,10 @@
+// Last updated: 7/28/2026, 1:34:03 PM
+1class Solution {
+2public:
+3    bool canWinNim(int n) {
+4        if(n%4==0){
+5            return false;
+6        }
+7        return true;
+8    }
+9};
