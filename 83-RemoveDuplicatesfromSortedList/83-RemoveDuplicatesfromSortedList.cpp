63d5108073e7c0ca2ff7f08370485c38493c2af9
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-08-07 14:05:02 +0530

    File updated at 8/7/2026, 2:05:02 PM

--- 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.cpp
+++ 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.cpp
@@ -1,23 +1,20 @@
-// Last updated: 8/4/2026, 1:26:35 PM
+// Last updated: 8/7/2026, 2:05:02 PM
 1class Solution {
 2public:
-3    ListNode* deleteDuplicates(ListNode* head) {
+3    ListNode* reverseList(ListNode* head) {
 4
-5        if(head == NULL)
-6            return head;
-7
-8        ListNode* current = head;
-9
-10        while(current != NULL && current->next != NULL){
-11
-12            if(current->val == current->next->val){
-13                current->next = current->next->next;
-14            }
-15            else{
-16                current = current->next;
-17            }
-18        }
-19
-20        return head;
-21    }
-22};
+5        ListNode *prev = nullptr;
+6        ListNode *curr = head;
+7        ListNode *next = nullptr;
+8
+9        while(curr != nullptr)
+10        {
+11            next = curr->next;
+12            curr->next = prev;
+13            prev = curr;
+14            curr = next;
+15        }
+16
+17        return prev;
+18    }
+19};
