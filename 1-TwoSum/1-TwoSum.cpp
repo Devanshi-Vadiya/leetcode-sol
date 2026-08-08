fd116c8ef004237295c72ae35aea8bd773a8be3a
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-08-08 16:40:46 +0530

    File updated at 8/8/2026, 4:40:46 PM

--- 1-TwoSum/1-TwoSum.cpp
+++ 1-TwoSum/1-TwoSum.cpp
@@ -1,22 +1,24 @@
-// Last updated: 8/8/2026, 4:39:48 PM
+// Last updated: 8/8/2026, 4:40:46 PM
 1class Solution {
 2public:
-3    bool hasCycle(ListNode *head) {
+3    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
 4
-5        ListNode *slow = head;
-6        ListNode *fast = head;
+5        ListNode *p1 = headA;
+6        ListNode *p2 = headB;
 7
-8        while(fast != nullptr && fast->next != nullptr)
+8        while(p1 != p2)
 9        {
-10            slow = slow->next;
-11            fast = fast->next->next;
-12
-13            if(slow == fast)
-14            {
-15                return true;
-16            }
-17        }
-18
-19        return false;
-20    }
-21};
+10            if(p1 == nullptr)
+11                p1 = headB;
+12            else
+13                p1 = p1->next;
+14
+15            if(p2 == nullptr)
+16                p2 = headA;
+17            else
+18                p2 = p2->next;
+19        }
+20
+21        return p1;
+22    }
+23};
