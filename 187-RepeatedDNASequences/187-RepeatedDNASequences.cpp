9dcc4db973bf52404bac832ad25574a5de82f1d6
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-08-12 14:18:23 +0530

    File updated at 8/12/2026, 2:18:22 PM

--- 187-RepeatedDNASequences/187-RepeatedDNASequences.cpp
+++ 187-RepeatedDNASequences/187-RepeatedDNASequences.cpp
@@ -1,20 +1,34 @@
-// Last updated: 8/12/2026, 2:17:38 PM
+// Last updated: 8/12/2026, 2:18:22 PM
 1class Solution {
 2public:
-3    vector<string> findRepeatedDnaSequences(string s) {
-4        unordered_map<string, int> mp;
-5        vector<string> ans;
-6
-7        for (int i = 0; i + 10 <= s.size(); i++) {
-8            string sub = s.substr(i, 10);
-9            mp[sub]++;
+3    ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+4        
+5        ListNode* prev = list1;
+6        
+7        // Move to node before index a
+8        for (int i = 0; i < a - 1; i++) {
+9            prev = prev->next;
 10        }
-11
-12        for (auto it : mp) {
-13            if (it.second > 1)
-14                ans.push_back(it.first);
-15        }
-16
-17        return ans;
-18    }
-19};
+11        
+12        ListNode* after = prev;
+13        
+14        // Move to node at index b
+15        for (int i = 0; i <= b - a + 1; i++) {
+16            after = after->next;
+17        }
+18        
+19        // Find last node of list2
+20        ListNode* tail = list2;
+21        while (tail->next != nullptr) {
+22            tail = tail->next;
+23        }
+24        
+25        // Connect list1 -> list2
+26        prev->next = list2;
+27        
+28        // Connect list2 -> remaining list1
+29        tail->next = after;
+30        
+31        return list1;
+32    }
+33};
