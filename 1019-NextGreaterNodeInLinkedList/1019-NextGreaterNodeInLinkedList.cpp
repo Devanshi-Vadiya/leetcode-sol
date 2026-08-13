bfebd387a038a2e1228e93817d21d2bc87dae1e9
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-08-13 13:34:09 +0530

    File updated at 8/13/2026, 1:34:08 PM

--- 1019-NextGreaterNodeInLinkedList/1019-NextGreaterNodeInLinkedList.cpp
+++ 1019-NextGreaterNodeInLinkedList/1019-NextGreaterNodeInLinkedList.cpp
@@ -1,35 +1,25 @@
-// Last updated: 8/13/2026, 1:31:44 PM
-1/**
-2 * Definition for singly-linked list.
-3 * struct ListNode {
-4 *     int val;
-5 *     ListNode *next;
-6 *     ListNode() : val(0), next(nullptr) {}
-7 *     ListNode(int x) : val(x), next(nullptr) {}
-8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-9 * };
-10 */
-11class Solution {
-12public:
-13    vector<int> nextLargerNodes(ListNode* head) {
-14        vector<int> ans;  //empty vector jo store kaega answer haar ek noode ka 
-15        ListNode* curr = head; //create karega pointer curr, jo head se start hoke haar node meh jayega
-16
-17        while (curr != nullptr) { //Loop run karega till curr end of nullptr tak nhi pahuch jata
-18            ListNode* temp = curr->next; //another pointer temp jo start hoga next node after curr
-19            int check = 0; //as the grater wla ko store karne ke liye
-20
-21            while (temp != nullptr) {
-22                if (temp->val > curr->val) { //compares karega value of temp with curr
-23                    check = temp->val; //agar large value milega to stor ein check
-24                    break; //stop kar dega search bcz hame chahiye 1st greater node aur largest nhi
-25                }
-26                temp = temp->next; //agar nhi mila toh
-27            }
-28
-29            ans.push_back(check);
-30            curr = curr->next; 
-31        }
-32        return ans; 
-33    }
-34};
+// Last updated: 8/13/2026, 1:34:08 PM
+1class Solution {
+2public:
+3    vector<int> nextLargerNodes(ListNode* head) {
+4        vector<int> a, ans;
+5        stack<int> st;
+6
+7        while(head) {
+8            a.push_back(head->val);
+9            head = head->next;
+10        }
+11
+12        ans.resize(a.size());
+13
+14        for(int i = 0; i < a.size(); i++) {
+15            while(!st.empty() && a[i] > a[st.top()]) {
+16                ans[st.top()] = a[i];
+17                st.pop();
+18            }
+19            st.push(i);
+20        }
+21
+22        return ans;
+23    }
+24};
