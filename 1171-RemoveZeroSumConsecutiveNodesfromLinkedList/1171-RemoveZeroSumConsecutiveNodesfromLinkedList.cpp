86df7320d14b4e25b6ec8ffa9b49913100b8a28d
Devanshi Vadiya <devanshivadiya@gmail.com>
2026-08-19 14:07:39 +0530

    Create 1171-RemoveZeroSumConsecutiveNodesfromLinkedList

---
+++ 1171-RemoveZeroSumConsecutiveNodesfromLinkedList/1171-RemoveZeroSumConsecutiveNodesfromLinkedList.cpp
@@ -0,0 +1,23 @@
+// Last updated: 8/19/2026, 2:07:40 PM
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* middleNode(ListNode* head) {
+14        ListNode * fast = head ;
+15        ListNode * slow = head ;
+16       while(fast != nullptr && fast->next != nullptr){
+17            fast = fast->next->next ;
+18            slow = slow->next ;
+19        } 
+20        return slow ;
+21    }
+22};
